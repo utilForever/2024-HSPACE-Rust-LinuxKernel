ba474add45979da3f482bdabd0689f1696f7c9df
Chris Ohk <utilforever@gmail.com>
2024-07-14 09:33:00 +0900

    feat: Update assignment 1-1

---
+++ 3 - Assignment/240714 - Rust Basic + Linux Kernel, Week 2, Day 2/fibonacci/src/lib.rs
@@ -0,0 +1,38 @@
+pub fn fib(n: u32) -> u32 {
+    if n <= 2 {
+        // The base case
+        todo!("Implement this");
+    } else {
+        // The recursive case
+        todo!("Implement this");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn fib_1() {
+        let ret = fib(1);
+        assert_eq!(ret, 1);
+    }
+
+    #[test]
+    fn fib_2() {
+        let ret = fib(2);
+        assert_eq!(ret, 1);
+    }
+
+    #[test]
+    fn fib_5() {
+        let ret = fib(5);
+        assert_eq!(ret, 5);
+    }
+
+    #[test]
+    fn fib_20() {
+        let ret = fib(20);
+        assert_eq!(ret, 6765);
+    }
+}
