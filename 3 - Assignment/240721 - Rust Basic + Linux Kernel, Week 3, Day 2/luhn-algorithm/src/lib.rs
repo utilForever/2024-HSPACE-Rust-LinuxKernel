51dfcdb3455e03dee1e46a05ca70535ab7c68423
Chris Ohk <utilforever@gmail.com>
2024-07-21 18:46:24 +0900

    feat: Update assignment 2-2

---
+++ 3 - Assignment/240721 - Rust Basic + Linux Kernel, Week 3, Day 2/luhn-algorithm/src/lib.rs
@@ -0,0 +1,68 @@
+pub fn luhn(cc_number: &str) -> bool {
+    let mut sum = 0;
+    let mut double = false;
+
+    for c in cc_number.chars().rev() {
+        if let Some(digit) = c.to_digit(10) {
+            if double {
+                let double_digit = digit * 2;
+                sum += if double_digit > 9 {
+                    double_digit - 9
+                } else {
+                    double_digit
+                };
+            } else {
+                sum += digit;
+            }
+
+            double = !double;
+        } else {
+            continue;
+        }
+    }
+
+    sum % 10 == 0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_valid_cc_number() {
+        assert!(luhn("4263 9826 4026 9299"));
+        assert!(luhn("4539 3195 0343 6467"));
+        assert!(luhn("7992 7398 713"));
+    }
+
+    #[test]
+    fn test_invalid_cc_number() {
+        assert!(!luhn("4223 9826 4026 9299"));
+        assert!(!luhn("4539 3195 0343 6476"));
+        assert!(!luhn("8273 1232 7352 0569"));
+    }
+
+    #[test]
+    fn test_non_digit_cc_number() {
+        assert!(!luhn("foo"));
+        assert!(!luhn("foo 0 0"));
+    }
+
+    #[test]
+    fn test_empty_cc_number() {
+        assert!(!luhn(""));
+        assert!(!luhn(" "));
+        assert!(!luhn("  "));
+        assert!(!luhn("    "));
+    }
+
+    #[test]
+    fn test_single_digit_cc_number() {
+        assert!(!luhn("0"));
+    }
+
+    #[test]
+    fn test_two_digit_cc_number() {
+        assert!(luhn(" 0 0 "));
+    }
+}
