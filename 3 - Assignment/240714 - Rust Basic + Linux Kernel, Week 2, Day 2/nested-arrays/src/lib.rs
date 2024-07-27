cc0fa34ffc80a034951621e143908a00f3999426
gudtldn <gudtldn1234@gmail.com>
2024-07-27 07:29:41 +0000

    solved nested arrays

--- 3 - Assignment/240714 - Rust Basic + Linux Kernel, Week 2, Day 2/nested-arrays/src/lib.rs
+++ 3 - Assignment/240714 - Rust Basic + Linux Kernel, Week 2, Day 2/nested-arrays/src/lib.rs
@@ -1,5 +1,9 @@
 pub fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
-    todo!("Implement this")
+    [
+        [matrix[0][0], matrix[1][0], matrix[2][0]],
+        [matrix[0][1], matrix[1][1], matrix[2][1]],
+        [matrix[0][2], matrix[1][2], matrix[2][2]],
+    ]
 }
 
 #[cfg(test)]
