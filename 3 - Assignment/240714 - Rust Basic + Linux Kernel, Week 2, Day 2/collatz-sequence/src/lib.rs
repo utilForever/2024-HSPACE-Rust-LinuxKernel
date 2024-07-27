2abd0e7ab275e40df218d9d563c3c43122229854
gudtldn <gudtldn1234@gmail.com>
2024-07-27 05:18:31 +0000

    solved Collatz Sequence

--- 3 - Assignment/240714 - Rust Basic + Linux Kernel, Week 2, Day 2/collatz-sequence/src/lib.rs
+++ 3 - Assignment/240714 - Rust Basic + Linux Kernel, Week 2, Day 2/collatz-sequence/src/lib.rs
@@ -1,6 +1,15 @@
 /// Determine the length of the collatz sequence beginning at `n`.
 pub fn collatz_length(mut n: i32) -> u32 {
-    todo!("Implement this")
+    let mut length = 1_u32;
+    while n != 1 {
+        if n % 2 == 0 {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        length += 1;
+    }
+    length
 }
 
 #[cfg(test)]
